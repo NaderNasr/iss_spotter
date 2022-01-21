076704327129a1b827e3c81b2b16e52607b0ef82
Nader Nasr <nadernasr7@gmail.com>
2022-01-20 19:01:47 -0500

    find geo location by ip

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const { fetchMyIP } = require('./iss');
+const {
+  fetchMyIP,
+  fetchCoordsByIP
+} = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -7,4 +10,13 @@ fetchMyIP((error, ip) => {
   }
 
   console.log('It worked! Returned IP:' , ip);
+});
+
+fetchCoordsByIP('99.230.114.180', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned coordinates:' , coordinates);
 });
